64b3c88b2a88702c96dfd0d22fe810c53a6fee5f
T3M100 . <youssef.elmehrzy0@gmail.com>
2026-05-12 04:52:56 +0300

    updates

--- src/layouts/JobSeekerDashboardLayout.tsx
+++ src/layouts/JobSeekerDashboardLayout.tsx
@@ -1,14 +1,20 @@
 import { Outlet } from "react-router-dom";
 
 import { JobSeekerAppSidebar } from "@/components/job-seeker-dashboard/JobSeekerAppSidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 export function JobSeekerDashboardLayout() {
 	return (
 		<SidebarProvider>
 			<JobSeekerAppSidebar />
-			<main className="flex w-full min-w-0 flex-1 flex-col bg-gray-100 p-4 sm:p-6 lg:px-8 lg:py-6">
-				<Outlet />
+
+			<main className=" relative flex w-full min-w-0 flex-1 flex-col bg-gray-100 ">
+				<div className=" sticky top-[68.89px]  z-10 bg-white p-3 shadow-sm      ">
+					<SidebarTrigger className="bg-white shadow-sm p-2 rounded-lg" />
+				</div>
+				<div className="p-4 sm:p-6 lg:px-8 lg:py-6">
+					<Outlet />
+				</div>
 			</main>
 		</SidebarProvider>
 	);
