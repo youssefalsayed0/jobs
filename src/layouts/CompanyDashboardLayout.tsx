135eabf53584ea49d9f1fad2293d44b25eb1d467
T3M100 . <youssef.elmehrzy0@gmail.com>
2026-05-12 04:57:00 +0300

    updates

--- src/layouts/CompanyDashboardLayout.tsx
+++ src/layouts/CompanyDashboardLayout.tsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom";
 import { CompanyAppSidebar } from "@/components/company-dashboard/CompanyAppSidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 export function CompanyDashboardLayout() {
 	return (
 		<SidebarProvider>
 			<CompanyAppSidebar />
-			<main className="flex w-full min-w-0 flex-1 flex-col bg-gray-100 p-4 sm:p-6 lg:px-8 lg:py-6">
-				<Outlet />
+			<main className=" relative flex w-full min-w-0 flex-1 flex-col bg-gray-100 ">
+				<div className=" sticky top-[68.89px]  z-10 bg-white p-3 shadow-sm      ">
+					<SidebarTrigger className="bg-white shadow-sm p-2 rounded-lg" />
+				</div>
+				<div className="p-4 sm:p-6 lg:px-8 lg:py-6">
+					<Outlet />
+				</div>
 			</main>
 		</SidebarProvider>
 	);
